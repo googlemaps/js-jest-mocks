a65b8c5e64975acc8c6e56f517ac6fe2f5d22ae3
Justin Poehnelt <jpoehnelt@google.com>
2021-06-17 19:24:32 +0000

    chore: init

---
+++ README.md
@@ -0,0 +1,44 @@
+# Jest Mocks for Google Maps
+
+[![npm](https://img.shields.io/npm/v/@googlemaps/three)](https://www.npmjs.com/package/@googlemaps/jest-mocks)
+![Build](https://github.com/googlemaps/js-jest-mocks/workflows/Build/badge.svg)
+![Release](https://github.com/googlemaps/js-jest-mocks/workflows/Release/badge.svg)
+[![codecov](https://codecov.io/gh/googlemaps/js-jest-mocks/branch/main/graph/badge.svg)](https://codecov.io/gh/googlemaps/js-jest-mocks)
+![GitHub contributors](https://img.shields.io/github/contributors/googlemaps/js-jest-mocks?color=green)
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+[![](https://github.com/jpoehnelt/in-solidarity-bot/raw/main/static//badge-flat.png)](https://github.com/apps/in-solidarity)
+[![Discord](https://img.shields.io/discord/676948200904589322?color=6A7EC2&logo=discord&logoColor=ffffff)](https://discord.gg/jRteCzP)
+
+
+## Description
+
+Jest mocks for Google Maps in TypeScript.
+
+**Note:** Currently this is a partial set of mocks/stubs.
+
+**Note:** This repository was previously part of https://github.com/googlemaps/v3-utility-library.
+
+## NPM
+
+Available via NPM as the package `@googlemaps/jest-mocks`
+
+## Example
+
+```typescript
+import { initialize } from "@googlemaps/jest-mocks";
+
+beforeEach(() => {
+  initialize();
+});
+
+```
+
+## Support
+
+This library is community supported. We're comfortable enough with the stability and features of
+the library that we want you to build real production applications on it.
+
+If you find a bug, or have a feature suggestion, please [log an issue][issues]. If you'd like to
+contribute, please read [How to Contribute][contrib].
+
+[issues]: https://github.com/googlemaps/v3-utility-library/issues
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { LatLng, LatLngBounds } from "./latlng";
+import { Map_ } from "./map";
+import { Marker } from "./marker";
+import { MVCArray } from "./mvcarray";
+import { MVCObject } from "./mvcobject";
+import { Point } from "./point";
+import { Size } from "./size";
+import { Polyline } from "./polyline";
+
+enum MapTypeId {
+  HYBRID = "hybrid",
+  ROADMAP = "roadmap",
+  SATELLITE = "satellite",
+  TERRAIN = "terrain",
+}
+
+const initialize = function (): void {
+  (global as any).google = {
+    maps: {
+      ImageMapType: jest.fn(),
+      Marker: Marker,
+      Map: Map_,
+      Point: Point,
+      Size: Size,
+      MVCObject: MVCObject,
+      MVCArray: MVCArray,
+      MapTypeId: MapTypeId,
+      LatLng: LatLng,
+      LatLngBounds: LatLngBounds,
+      event: {
+        addListener: jest.fn(),
+        addListenerOnce: jest.fn(),
+        addDomListerner: jest.fn(),
+        addDomListernerOnce: jest.fn(),
+        clearInstanceListeners: jest.fn(),
+        clearListeners: jest.fn(),
+        removeListener: jest.fn(),
+        trigger: jest.fn(),
+      },
+      Polyline: Polyline,
+    },
+  };
+};
+
+export {
+  Marker,
+  Map_ as Map,
+  Size,
+  MVCArray,
+  MVCObject,
+  Polyline,
+  LatLng,
+  LatLngBounds,
+  initialize,
+};
