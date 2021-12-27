cbca32ae749a0fa891e0f59513a3f3842155942d
Eric <43848365+eegli@users.noreply.github.com>
2021-12-27 09:51:07 +0100

    add street view service and enum mocks

---
+++ src/street-view/service/service.ts
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { LatLng } from '../../latlng';
+import { Size } from '../../size';
+
+export class StreetViewService implements google.maps.StreetViewService {
+  getPanorama = jest
+    .fn()
+    .mockImplementation(
+      (
+        request:
+          | google.maps.StreetViewLocationRequest
+          | google.maps.StreetViewPanoRequest,
+        callback?: (
+          a: google.maps.StreetViewPanoramaData | null,
+          b: google.maps.StreetViewStatus
+        ) => void
+      ): Promise<google.maps.StreetViewResponse> => {
+        return Promise.resolve({
+          data: {
+            copyright: 'copyright',
+            imageDate: 'imageDate',
+            links: [],
+            location: {
+              description: 'description',
+              pano: '123',
+              shortDescription: null,
+              latLng: new LatLng(1, 1),
+            },
+            tiles: {
+              centerHeading: 0,
+              getTileUrl: () => 'tileUrl',
+              tileSize: new Size(1, 1),
+              worldSize: new Size(1, 1),
+            },
+          },
+        });
+      }
+    );
+}
