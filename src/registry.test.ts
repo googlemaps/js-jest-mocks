a8b17e7f8a9a2b15abd4aa292c00d886cab192d1
Eric <43848365+eegli@users.noreply.github.com>
2022-01-27 11:32:50 +0100

    first draft for registry

---
+++ src/registry.test.ts
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { initialize } from ".";
+import { MVCObject, Map } from ".";
+import { mockInstances, MOCKS_REGISTRY } from "./registry";
+
+test("mocks registry", () => {
+  initialize();
+  mockInstances.clearAll();
+  expect(MOCKS_REGISTRY.size).toBe(0);
+  const mvc1 = new google.maps.MVCObject();
+  const mvc2 = new google.maps.MVCObject();
+  expect(MOCKS_REGISTRY.size).toBe(1);
+  expect(mockInstances.get(MVCObject)).toStrictEqual([mvc1, mvc2]);
+  const map = new google.maps.Map(null);
+  mockInstances.clear(MVCObject);
+  expect(mockInstances.get(MVCObject)).toStrictEqual([]);
+  expect(mockInstances.get(Map)).toStrictEqual([map]);
+  mockInstances.clearAll();
+  expect(MOCKS_REGISTRY.size).toBe(0);
+});
